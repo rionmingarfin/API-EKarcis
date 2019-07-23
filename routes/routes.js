55469bf519f357aba85c954fbdb3e848a382d731
rionmingarfin <43402837+rionmingarfin@users.noreply.github.com>
2019-07-23 14:09:49 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,7 @@
 
 module.exports = function (app) {
 
-	const controller = require('../controler/category')
-	const auth = require('../controler/auth')
-
+    const auth = require('../controler/auth')
     const controller = require('../controler/category')   
     const controllerTour =require('../controler/tour')
     const response = require('../response/response');
@@ -26,7 +24,6 @@ module.exports = function (app) {
     // Auth
     app.post('/auth_login', auth.login)
     app.post('/auth_register', auth.register)
-=======
     //tour
     
     app.get('/tour',controllerTour.getTour)
